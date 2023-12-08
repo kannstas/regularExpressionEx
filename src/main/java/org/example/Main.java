319ba64dd963ba4d4271e3ca0621cee5ead09f28
kannstas <k.annstas@gmail.com>
2023-12-08 12:45:22 +0300

    new examples

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,19 +1,107 @@
 package org.example;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.IntStream;
+
+
 public class Main {
     public static void main(String[] args) {
-        // Press Opt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        String s1 = "ABCD ABCE ABCDJ ABCDFF";
+        String s2 = "ABCCDEFF";
+        String s3 = "ABCOP";
+        String s4 = "abcd abce a0bc5abcg6abch? st st123 st0 12345 ababwef abab ababab ababababababab";
+        String s5 = "мой номер 79804357234";
 
-        // Press Ctrl+R or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        Pattern pattern1 = Pattern.compile("AB");
+        Pattern pattern2 = Pattern.compile("[ABC]");
+        Pattern pattern3 = Pattern.compile("AB[A-F]OP");
+        Pattern pattern4 = Pattern.compile("abc[^e-g4-7]");
+        Pattern pattern5 = Pattern.compile("abc(e|5)");
+        Pattern pattern6 = Pattern.compile("abc.");
+        Pattern pattern7 = Pattern.compile("abch$");
 
-            // Press Ctrl+D to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Cmd+F8.
-            System.out.println("i = " + i);
+        Pattern pattern8 = Pattern.compile("[0-9]");
+        Pattern pattern81 = Pattern.compile("\\d"); // только цифры
+        Pattern pattern82 = Pattern.compile("\\D"); // всё, кроме цифр
+
+        Pattern pattern83 = Pattern.compile("\\w"); // только цифры и буквы
+        Pattern pattern84 = Pattern.compile("\\W"); // всё, кроме цифр и букв
+
+        Pattern pattern85 = Pattern.compile("st\\w+");
+
+        Pattern pattern86 = Pattern.compile("\\w{5}");
+
+        Pattern pattern87 = Pattern.compile("\\w\\s\\w");
+        Pattern pattern88 = Pattern.compile("\\D{2,}");
+
+        Pattern pattern89 = Pattern.compile("(ab){2,6}");
+
+
+        Matcher matcher = pattern89.matcher(s4);
+
+        while (matcher.find()) {
+            System.out.println("position " + matcher.start() + " " + matcher.group() + "\n");
+        }
+        System.out.println("end");
+
+
+        System.out.println(s5.replaceAll("\\d", "*"));
+
+        returnNumbersFromAString("43fdsw56hhfgff8");
+
+        System.out.println(returnListOfPhoneNumbers("+79515604647"));
+
+        System.out.println(replaceNumbersInStringWithCharacters("rersds733291023rkre0483i2324"));
+
+        System.out.println(countOfNumbersInString("674red27a11d"));
+    }
+
+
+    public static void returnNumbersFromAString(String stringWithNumbers) {
+
+        Pattern pattern = Pattern.compile("\\d");
+
+        Matcher matcher = pattern.matcher(stringWithNumbers);
+
+        while (matcher.find()) {
+            System.out.println("position " + matcher.start() + " number " + matcher.group());
         }
     }
+
+    public static List<String> returnListOfPhoneNumbers(String phoneNumber) {
+
+        List<String> phoneNumbersList = new ArrayList<>();
+        Pattern pattern = Pattern.compile("\\+\\d{0,11}");
+
+        Matcher matcher = pattern.matcher(phoneNumber);
+
+        while (matcher.find()) {
+            phoneNumbersList.add(matcher.group());
+        }
+        return phoneNumbersList;
+    }
+
+
+    public static String replaceNumbersInStringWithCharacters(String stringWithNumbers) { //метод берет строку и заменяет числа на *
+        return stringWithNumbers.replaceAll("\\d", "*");
+    }
+
+
+    public static int countOfNumbersInString(String stringWithNumbers) {
+
+        Pattern pattern = Pattern.compile("\\d");
+        Matcher matcher = pattern.matcher(stringWithNumbers);
+        int counter = 0;
+        while (matcher.find()) {
+            counter++; // метод groupCount() не подойдет для подобного подсчета, поэтому надо создать счетчик
+        }
+
+        return counter;
+    }
+
+
 }
